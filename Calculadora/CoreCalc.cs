8c926e1b06d093587465311d7a418f0163bf0024
Alex Gregori Tomás <reproteq@gmail.com>
2020-10-20 10:35:48 +0200

    calc buttons

--- Calculadora/CoreCalc.cs
+++ Calculadora/CoreCalc.cs
@@ -8,47 +8,16 @@ using System.Windows;
 
 namespace Calculadora
 {
-    class CoreCalc
+    public partial class CoreCalc
     {
 
-        public double Resultado;
 
-        public Operacion miOperacion = new Operacion();
+
+
     }
 
 
 
 
-
-    class Operacion
-    {
-
-    }
-
-
-    class Suma
-    {
-        public static double suma(double n1, double n2)
-        {
-            MessageBox.Show("Suma.suma");
-            return n1 + n2;
-
-        }
-    }
-
-    class Resultado
-    {
-        public static double resultado(double n1, double n2, Operacion operacion)
-        {
-            double res = n1;
-         
-            return res;
-        }
-
-
-        public double n2;
-        public double n1;
-        public double res;
-    }
-
+ 
 }
--- Calculadora/MainWindow.xaml.cs
+++ Calculadora/MainWindow.xaml.cs
@@ -26,11 +26,184 @@ namespace Calculadora
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnCeroClick(object sender, RoutedEventArgs e)
         {
-            Suma.suma(12, 2);
-            MessageBox.Show("alert");
-
+            TBoxResultado.Text = "0";
         }
+
+
+        private void btnUnoClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "1";
+        }
+
+        private void btnDosClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "2";
+        }
+
+        private void btnTresClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "3";
+        }
+
+        private void btnCuatroClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "4";
+        }
+        private void btnCincoClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "5";
+        }
+        private void btnSeisClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "6";
+        }
+        private void btnSieteClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "7";
+        }
+        private void btnOchoClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "8";
+        }
+        private void btnNueveClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "9";
+        }
+
+        private void btnSumaClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "+";
+        }
+
+        private void btnRestaClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "-";
+        }
+
+        private void btnMultiplicacionClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "x";
+        }
+
+        private void btnDivisionClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "/";
+        }
+
+        private void btnPorcentajeClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "%";
+        }
+
+        private void btnParentesisAbrirClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "(";
+        }
+
+        private void btnParentesisCerrarClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = ")";
+        }
+
+
+        private void btnFlechaIzClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "<-";
+        }
+
+
+        private void btnFlechaDerClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "->";
+        }
+
+        private void btnClearClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "Limpiar";
+        }
+
+        private void btnResultadoClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "=";
+        }
+
+        private void btnMasMenosClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "+ -";
+        }
+
+        private void btnComaClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = ",";
+        }
+
+        private void btnAClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "A";
+        }
+        private void btnBClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "B";
+        }
+
+        private void btnCClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "C";
+        }
+
+        private void btnDClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "D";
+        }
+
+        private void btnEClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "E";
+        }
+
+        private void btnFClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "F";
+        }
+
+
+        private void btnHexClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "Hex";
+        }
+        private void btnDecClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "Dec";
+        }
+
+        private void btnOctClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "Oct";
+        }
+
+        private void btnBinClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "Bin";
+        }
+
+        private void btnAtrasClick(object sender, RoutedEventArgs e)
+        {
+            TBoxResultado.Text = "Atras";
+        }
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
 }
