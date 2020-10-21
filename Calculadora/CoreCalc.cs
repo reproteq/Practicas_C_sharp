36593570db8e9504cd7a6fa62ff2288732933adb
Alex Gregori Tomás <reproteq@gmail.com>
2020-10-21 17:19:55 +0200

    calculadora ok

--- Calculadora/CoreCalc.cs
+++ Calculadora/CoreCalc.cs
@@ -1,23 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Globalization;
 
 namespace Calculadora
 {
-    public partial class CoreCalc
+    class CoreCalc 
     {
+        public double coreCalc(string a, string b, string c)
+        {
+
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            
+
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ",";
+            provider.NumberGroupSeparator = ".";
+            provider.NumberGroupSizes = new int[] { 3 };
+
+
+            switch (c)
+            {
+                case "-":
+                    //resta
+                    A = Convert.ToDouble(a, provider);
+                    B = Convert.ToDouble(b, provider);
+                    resul = B - A;
+                   // MessageBox.Show("resta" + A + c + B + "=" + resul);
+
+                    break;
+
+                case "+":
+                    //suma
+                    A = Convert.ToDouble(a, provider);
+                    B = Convert.ToDouble(b, provider);
+                    resul = A + B;
+                   // MessageBox.Show("suma" + A + c + B + "=" + resul);
+
+                    break;
+
+                case "x":
+                    //multi
+                    A = Convert.ToDouble(a, provider);
+                    B = Convert.ToDouble(b, provider);
+                    resul = A * B;                    
+                    break;
+
+                case "/":
+                    //div
+                    A = Convert.ToDouble(a, provider);
+                    B = Convert.ToDouble(b, provider);
+                    resul = B / A;
+                    break;
+
+                case "%":
+                    //  number = (percentage / 100) * totalNumber;
+                    A = Convert.ToDouble(a, provider);
+                    B = Convert.ToDouble(b, provider);
+                    resul = ((B / 100) * A);
+                    break;
+
+            }
+
+            return resul;
+        }
 
 
 
+
+        private double A;
+        private double B;
+        
+        private double resul;
+        private string a;
+        private string b;
+        private string c; // simbolo
+    }
+
+
+
+    class Atras
+    {
+        public string atras(string str)
+        {
+            this.str = str;
+            lastChar = str.Remove(str.Length - 1, 1);
+
+
+            return lastChar;
+        }
+
+        private string str;
+        private string lastChar;
+    }
+
+
+
+    class TboxA
+    {
+        public string TboxAres 
+        {
+            get => tboxAres; 
+            set => tboxAres = value;
+        }
+        private string tboxAres;
+        ~TboxA()
+        {
+            Console.WriteLine("Destroy A");
+        }
+    }
+
+ 
+
+    class TboxB
+    {
+        public string TboxBres
+        {
+            get => tboxBres;
+            set => tboxBres = value;
+        }
+        private string tboxBres;
+        ~TboxB()
+        {
+            Console.WriteLine("Destroy B");
+        }
+
+    }
+
+    class TboxC
+    {
+        public string TboxCres
+        {
+            get => tboxCres;
+            set => tboxCres = value;
+        }
+        private string tboxCres;
+        ~TboxC()
+        {
+            Console.WriteLine("Destroy C");
+        }
 
     }
 
 
 
-
- 
 }
--- Calculadora/MainWindow.xaml.cs
+++ Calculadora/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls.WebParts;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -24,174 +27,320 @@ namespace Calculadora
         public MainWindow()
         {
             InitializeComponent();
+            
+
         }
+
+        CoreCalc miCoreCalc = new CoreCalc();
+        Atras miAtras = new Atras();
+        TboxA miTboxA = new TboxA();
+        TboxB miTboxB = new TboxB();
+        TboxC miTboxC = new TboxC();
+             
+
+        // click botones de la calculadora
 
         private void btnCeroClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "0";
+            miTboxA.TboxAres += "0";
+            TBoxA.Text = miTboxA.TboxAres;
+          
+            
         }
 
 
         private void btnUnoClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "1";
+            miTboxA.TboxAres +="1";
+            TBoxA.Text = miTboxA.TboxAres; 
         }
 
         private void btnDosClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "2";
+            miTboxA.TboxAres += "2";
+            TBoxA.Text = miTboxA.TboxAres;
         }
 
         private void btnTresClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "3";
+            miTboxA.TboxAres += "3";
+            TBoxA.Text = miTboxA.TboxAres;
         }
 
         private void btnCuatroClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "4";
+            miTboxA.TboxAres += "4";
+            TBoxA.Text = miTboxA.TboxAres;
         }
         private void btnCincoClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "5";
+            miTboxA.TboxAres += "5";
+            TBoxA.Text = miTboxA.TboxAres;
         }
         private void btnSeisClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "6";
+            miTboxA.TboxAres += "6";
+            TBoxA.Text = miTboxA.TboxAres;
         }
         private void btnSieteClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "7";
+            miTboxA.TboxAres += "7";
+            TBoxA.Text = miTboxA.TboxAres;
         }
         private void btnOchoClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "8";
+            miTboxA.TboxAres += "8";
+            TBoxA.Text = miTboxA.TboxAres;
         }
         private void btnNueveClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "9";
+            miTboxA.TboxAres += "9";
+            TBoxA.Text = miTboxA.TboxAres;
+        }
+
+        // ataras
+        private void btnAtrasClick(object sender, RoutedEventArgs e)
+        {
+            // TBoxA.Text = "Atras";
+            if (TBoxA.Text != "") 
+            { 
+                TBoxA.Text = miTboxA.TboxAres;
+                var atr = miAtras.atras(TBoxA.Text);
+                TBoxA.Text = atr;
+                 miTboxA.TboxAres = atr;
+            }
+            else { MessageBox.Show("No hay nada que borrar , Por favor introduce un numero"); };
+        }
+
+        // decimals
+
+        private void btnComaClick(object sender, RoutedEventArgs e)
+        {
+            miTboxA.TboxAres += ",";
+            TBoxA.Text = miTboxA.TboxAres;
+           // miRegEx.regex(".", TBoxA.Text);
+        }
+
+        // operaciones
+        private void btnClearClick(object sender, RoutedEventArgs e)
+        {
+            // TBoxA.Text = "Limpiar";
+            TBoxA.Text = ""; // limpiamos el tboxa para  poder poner el otro numero para poder operar
+            miTboxA.TboxAres = "";
+
+            TBoxB.Text = ""; // limpiamos el tboxb para  poder poner el otro numero para poder operar
+            miTboxB.TboxBres = "";
+
+            TBoxC.Text = ""; // limpiamos el tboxc para  poder poner el otro numero para poder operar
+            miTboxC.TboxCres = "";
+
+        }
+
+        //operacion resultado simbolo = 
+        private void btnResultadoClick(object sender, RoutedEventArgs e)
+        {
+            // miTboxA.TboxAres += "=";
+            if (TBoxA.Text != "")
+            {
+                double res = miCoreCalc.coreCalc(miTboxA.TboxAres, miTboxB.TboxBres, miTboxC.TboxCres);
+                TBoxA.Text = res.ToString();
+                miTboxA.TboxAres = res.ToString();
+            }
+            else { MessageBox.Show("Por favor introduce un numero"); };
+            //limpiamos c b
+            TBoxB.Text = ""; // limpiamos el tboxb para  poder poner el otro numero para poder operar
+            miTboxB.TboxBres = "";
+
+            TBoxC.Text = ""; // limpiamos el tboxc para  poder poner el otro numero para poder operar
+            miTboxC.TboxCres = "";
+
+
         }
 
         private void btnSumaClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "+";
+            if (TBoxA.Text != "")
+            {
+                miTboxC.TboxCres = "+"; // seteamos la propiedad del objeto c con el operador que tiene  que guardar
+                TBoxC.Text = miTboxC.TboxCres; // le pasamos el valor al tbox c para que muestre el tipo de operacion que se va a realizar
+
+                miTboxB.TboxBres = miTboxA.TboxAres;  // seteamos la propiedad del objeto b con el valor que se piko en la calc pasando el valor de tboxA a tboxB          
+                TBoxB.Text = miTboxB.TboxBres; // le pasamos el valor a tbox b para que muestre el valor numerico que se piko y con el cual se va a operar
+
+                TBoxA.Text = ""; // limpiamos el tboxa para  poder poner el otro numero para poder operar
+                miTboxA.TboxAres = "";
+            }
+            else 
+            { 
+                MessageBox.Show("Por favor introduce un numero"); 
+            }
+
         }
 
         private void btnRestaClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "-";
+            if (TBoxA.Text != "")
+            {
+                miTboxC.TboxCres = "-";
+                TBoxC.Text = miTboxC.TboxCres;
+                // pasamos el valor de tboxa a tboxb
+                miTboxB.TboxBres = miTboxA.TboxAres;
+                TBoxB.Text = miTboxB.TboxBres;
+
+                TBoxA.Text = ""; // limpiamos el tboxa
+                miTboxA.TboxAres = "";
+            }
+            else 
+            { 
+                MessageBox.Show("Por favor introduce un numero"); 
+            }
         }
 
         private void btnMultiplicacionClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "x";
+            if (TBoxA.Text != "")
+            {
+                miTboxC.TboxCres = "x"; // seteamos la propiedad del objeto c con el operador que tiene  que guardar
+                TBoxC.Text = miTboxC.TboxCres; // le pasamos el valor al tbox c para que muestre el tipo de operacion que se va a realizar
+
+                miTboxB.TboxBres = miTboxA.TboxAres;  // seteamos la propiedad del objeto b con el valor que se piko en la calc pasando el valor de tboxA a tboxB          
+                TBoxB.Text = miTboxB.TboxBres; // le pasamos el valor a tbox b para que muestre el valor numerico que se piko y con el cual se va a operar
+
+                TBoxA.Text = ""; // limpiamos el tboxa para  poder poner el otro numero para poder operar
+                miTboxA.TboxAres = "";
+            }
+            else 
+            {
+                MessageBox.Show("Por favor introduce un numero");
+            }
         }
 
         private void btnDivisionClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "/";
+            if (TBoxA.Text != "")
+            {
+                miTboxC.TboxCres = "/"; // seteamos la propiedad del objeto c con el operador que tiene  que guardar
+                TBoxC.Text = miTboxC.TboxCres; // le pasamos el valor al tbox c para que muestre el tipo de operacion que se va a realizar
+
+                miTboxB.TboxBres = miTboxA.TboxAres;  // seteamos la propiedad del objeto b con el valor que se piko en la calc pasando el valor de tboxA a tboxB          
+                TBoxB.Text = miTboxB.TboxBres; // le pasamos el valor a tbox b para que muestre el valor numerico que se piko y con el cual se va a operar
+
+                TBoxA.Text = ""; // limpiamos el tboxa para  poder poner el otro numero para poder operar
+                miTboxA.TboxAres = "";
+            }
+            else 
+            { 
+                MessageBox.Show("Por favor introduce un numero"); 
+            }
         }
 
         private void btnPorcentajeClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "%";
+            if (TBoxA.Text != "")
+            {
+                miTboxC.TboxCres = "%"; // seteamos la propiedad del objeto c con el operador que tiene  que guardar
+                TBoxC.Text = miTboxC.TboxCres; // le pasamos el valor al tbox c para que muestre el tipo de operacion que se va a realizar
+
+                miTboxB.TboxBres = miTboxA.TboxAres;  // seteamos la propiedad del objeto b con el valor que se piko en la calc pasando el valor de tboxA a tboxB          
+                TBoxB.Text = miTboxB.TboxBres; // le pasamos el valor a tbox b para que muestre el valor numerico que se piko y con el cual se va a operar
+
+                TBoxA.Text = ""; // limpiamos el tboxa para  poder poner el otro numero para poder operar
+                miTboxA.TboxAres = "";
+
+            }
+            else 
+            { 
+                MessageBox.Show("Por favor introduce un numero"); 
+            }
         }
 
         private void btnParentesisAbrirClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "(";
+
+            TBoxA.Text = "(";
         }
 
         private void btnParentesisCerrarClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = ")";
+            TBoxA.Text = ")";
         }
 
 
         private void btnFlechaIzClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "<-";
+            TBoxA.Text = "<-";
         }
 
 
         private void btnFlechaDerClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "->";
+            TBoxA.Text = "->";
         }
 
-        private void btnClearClick(object sender, RoutedEventArgs e)
-        {
-            TBoxResultado.Text = "Limpiar";
-        }
 
-        private void btnResultadoClick(object sender, RoutedEventArgs e)
-        {
-            TBoxResultado.Text = "=";
-        }
+
 
         private void btnMasMenosClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "+ -";
+            TBoxA.Text = "+ -";
         }
 
-        private void btnComaClick(object sender, RoutedEventArgs e)
-        {
-            TBoxResultado.Text = ",";
-        }
-
+        // hex bin dec oct
         private void btnAClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "A";
+            miTboxA.TboxAres += "A";
+            TBoxA.Text = miTboxA.TboxAres;
         }
         private void btnBClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "B";
+            miTboxA.TboxAres += "B";
+            TBoxA.Text = miTboxA.TboxAres;
         }
 
         private void btnCClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "C";
+            miTboxA.TboxAres += "C";
+            TBoxA.Text = miTboxA.TboxAres;
         }
 
         private void btnDClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "D";
+            miTboxA.TboxAres += "D";
+            TBoxA.Text = miTboxA.TboxAres;
         }
 
         private void btnEClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "E";
+            miTboxA.TboxAres += "E";
+            TBoxA.Text = miTboxA.TboxAres;
         }
 
         private void btnFClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "F";
+            miTboxA.TboxAres += "F";
+            TBoxA.Text = miTboxA.TboxAres;
         }
 
 
         private void btnHexClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "Hex";
+            TBoxA.Text = "Hex";
         }
         private void btnDecClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "Dec";
+            TBoxA.Text = "Dec";
         }
 
         private void btnOctClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "Oct";
+            TBoxA.Text = "Oct";
         }
 
         private void btnBinClick(object sender, RoutedEventArgs e)
         {
-            TBoxResultado.Text = "Bin";
+            TBoxA.Text = "Bin";
         }
 
-        private void btnAtrasClick(object sender, RoutedEventArgs e)
-        {
-            TBoxResultado.Text = "Atras";
-        }
 
 
 
