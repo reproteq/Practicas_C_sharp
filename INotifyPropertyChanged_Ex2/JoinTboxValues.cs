cd44e7157f62ccd84fe65e5b12f99c268d8bca9a
Alex Gregori Tomás <reproteq@gmail.com>
2020-10-22 18:38:52 +0200

    inotify example2

---
+++ INotifyPropertyChanged_Ex2/JoinTboxValues.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifyPropertyChanged_Ex2
+{
+    // clase publica i con una interface Inotifypropertychanged para detectar cambios en propiedades de objetos
+    public class JoinTboxValues : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // metodo NotifyPropertyChanged es el encargado de detectar los cambios en las propiedades de los objetos que llevaran la llamada a este metodo
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        // declaracion de variables str o campos de clase
+        private string tbox1, tbox2, tbox3, tbox4;
+        internal object Tbox;
+
+        public string Tbox1 
+        {
+            get { return tbox1; }
+            set { tbox1 = value;
+                OnPropertyChanged("Tbox4"); // llamada al metodo onpropertychanged
+            }
+        }
+
+        public string Tbox2
+        {
+            get { return tbox2; }
+            set { tbox2 = value;
+                OnPropertyChanged("Tbox4");
+            }
+        }
+
+        public string Tbox3
+        {
+            get { return tbox3; }
+            set { tbox3 = value;
+                OnPropertyChanged("Tbox4");
+            }
+        }
+
+        public string Tbox4
+        {
+            get { return tbox1 + " " + tbox2 + " " + tbox3 ; }
+            set { tbox4 = value; }
+        }
+
+        
+    }
+
+
+}
---
+++ INotifyPropertyChanged_Ex2/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INotifyPropertyChanged_Ex2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // inicilizacion de la instancia de la clase jointboxvalues y seteo  de los campos de dicha clase
+            miJoinTboxValues = new JoinTboxValues { Tbox1="A" , Tbox2="B", Tbox3="C" };
+           // miJoinTboxValues = new JoinTboxValues { };
+
+            // datacontext de la instacia de la clase u objeto creado
+            this.DataContext = miJoinTboxValues;
+        }
+
+
+
+        // declaracion de propiedad de campo en este caso es un objeto creado en la class jointboxvalues.cs
+        JoinTboxValues miJoinTboxValues;
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+            // borra los campos del xaml
+             Textbox1.Text = "";
+             Textbox2.Text = "";
+             Textbox3.Text = "";
+             Textbox4.Text = "";
+             Textbox5.Text = "";
+            
+            // recorre el objeto i borra sus propiedades 
+            var s = miJoinTboxValues;
+            foreach (var p in s.GetType().GetProperties().Where(p => !p.GetGetMethod().GetParameters().Any()))
+            {
+                p.SetValue(s, "");                
+            }
+
+
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+            // recorre el objeto i extrae sus propiedades en consola y en msgbox
+            var s = miJoinTboxValues;
+            foreach (var p in s.GetType().GetProperties().Where(p => !p.GetGetMethod().GetParameters().Any()))
+
+            {
+                MessageBox.Show(p.GetValue(s, null).ToString());
+                Console.WriteLine(p.GetValue(s, null));
+            }
+        }
+
+
+
+
+
+    }
+
+
+
+}
